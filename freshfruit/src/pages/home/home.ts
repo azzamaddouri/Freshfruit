dce27a6e945c3339aedb9211f731fe70a6c928b4
Azza Maddouri <90465414+azzamaddouri@users.noreply.github.com>
2022-06-03 18:00:10 +0200

    Delete freshfruit directory

--- freshfruit/src/pages/home/home.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { FruitsPage } from '../fruits/fruits';
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
-export class HomePage {
-  fruitsPage = FruitsPage;
-}
